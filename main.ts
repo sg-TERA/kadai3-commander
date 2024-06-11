b4b987e815183502470a81ecf517d31572246944
Xx_ShuGO_xX <st22131sh@gm.ibaraki-ct.ac.jp>
2024-06-11 11:52:43 +0900

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    basic.showNumber(radio.receivedPacket(RadioPacketProperty.SignalStrength))
+})
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(5)
+})
+radio.setGroup(1)
+radio.sendNumber(0)
 basic.forever(function () {
 	
 })
