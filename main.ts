829e0c800711ef1b06457b063a03fa3e29b9b75a
Xx_ShuGO_xX <st22131sh@gm.ibaraki-ct.ac.jp>
2024-06-11 12:15:48 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,14 @@
 input.onButtonPressed(Button.A, function () {
     radio.sendNumber(1)
+    basic.showNumber(1)
 })
 input.onButtonPressed(Button.AB, function () {
     radio.sendNumber(3)
+    basic.showNumber(3)
 })
 input.onButtonPressed(Button.B, function () {
     radio.sendNumber(2)
+    basic.showNumber(2)
 })
 radio.setGroup(9)
 radio.sendNumber(123456789)
